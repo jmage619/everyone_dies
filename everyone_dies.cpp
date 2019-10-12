78fe9a77ae25bc6f5f294358c750dc7489cce7dd
Jeff <jmage619@gmail.com>
2019-10-12 00:52:56 -0700

    instantiate Sprite in call to c fun from lua

--- everyone_dies.cpp
+++ everyone_dies.cpp
@@ -4,11 +4,32 @@ extern "C" {
 	#include "lauxlib.h"
 }
 
+#include <iostream>
+using namespace std;
+
+class Sprite {
+public:
+    int health;
+};
+
+Sprite* sprite = nullptr;
+
 lua_State* L;
+
+int create_sprite(lua_State* L) {
+    sprite = new Sprite;
+
+    sprite->health = lua_tointeger(L, 1);
+    lua_pushlightuserdata(L, sprite);
+
+    return 1;
+}
 
 int main() {
     L = luaL_newstate();
     luaL_openlibs(L);
+
+    lua_register(L, "create_sprite", create_sprite);
 
     luaL_dofile(L, "script.lua");
 
@@ -16,5 +37,11 @@ int main() {
     lua_call(L, 0, 0);
 
     lua_close(L);
+
+    if (sprite != nullptr) {
+        cout << "sprite health: " << sprite->health << endl;
+        delete sprite;
+    }
+
     return 0;
 }
--- script.lua
+++ script.lua
@@ -1,3 +1,5 @@
 function do_something()
     print("lua called!!")
+    print("instantiating sprite...")
+    create_sprite(5)
 end
