f27e7129a0efaddf652e7093dbe34c7485a479ab
Jeff <jmage619@gmail.com>
2019-10-13 10:13:48 -0700

    memory manage sprites in a lua array instead

--- everyone_dies.cpp
+++ everyone_dies.cpp
@@ -1,7 +1,7 @@
 extern "C" {
-	#include "lua.h"
-	#include "lualib.h"
-	#include "lauxlib.h"
+#include "lua.h"
+#include "lualib.h"
+#include "lauxlib.h"
 }
 
 #include <cstdlib>
@@ -20,23 +20,18 @@ public:
     int health;
 };
 
-std::vector<std::unique_ptr<Sprite>> sprites;
-
 int sprite_new(lua_State* L) {
-    Sprite* sprite = new Sprite;
+    Sprite* sprite = static_cast<Sprite*>(lua_newuserdata(L, sizeof(Sprite)));
 
     lua_pushstring(L, "name");
-    lua_gettable(L, -2);
+    lua_gettable(L, -3);
     sprite->name = lua_tostring(L, -1);
     lua_pop(L, 1);
 
     lua_pushstring(L, "health");
-    lua_gettable(L, -2);
+    lua_gettable(L, -3);
     sprite->health = lua_tointeger(L, -1);
     lua_pop(L, 1);
-
-    lua_pushlightuserdata(L, sprite);
-    sprites.push_back(std::unique_ptr<Sprite>(sprite));
 
     return 1;
 }
@@ -75,22 +70,35 @@ int main() {
     luaL_setfuncs(L, sprite_lib, 0);
     lua_setglobal(L, "Sprite");
 
+    lua_newtable(L);
+    lua_setglobal(L, "sprites");
+
     luaL_dofile(L, "script.lua");
 
     lua_getglobal(L, "init");
     lua_call(L, 0, 0);
 
-    while (sprites.size() > 0) {
-        int index = rand() % sprites.size();
-        --sprites[index]->health;
+    while (true) {
+        lua_getglobal(L, "sprites");
+        int len = luaL_len(L, -1);
+        if (len == 0)
+            break;
+
+        int index = rand() % len + 1;
+        lua_rawgeti(L, -1, index);
+        Sprite* s = static_cast<Sprite*>(lua_touserdata(L, -1));
+        int health = --s->health;
+        lua_pop(L, 1);
 
         lua_getglobal(L, "handle_damage");
-        lua_pushlightuserdata(L, sprites[index].get());
+        lua_pushnumber(L, index);
         lua_call(L, 1, 0);
 
-        if (sprites[index]->health == 0) {
+        /*if (health == 0) {
             sprites.erase(sprites.begin() + index);
         }
+        */
+        lua_pop(L, 1); // pop sprites array
     }
 
     lua_close(L);
--- script.lua
+++ script.lua
@@ -1,18 +1,19 @@
 function init()
     print("init called!!")
     print("instantiating sprites...")
-    Sprite.new({name = "jerry", health = 5})
-    Sprite.new({name = "bob", health = 3})
-    Sprite.new({name = "joe", health = 8})
+    table.insert(sprites, Sprite.new({name = "jerry", health = 5}))
+    table.insert(sprites, Sprite.new({name = "bob", health = 3}))
+    table.insert(sprites, Sprite.new({name = "joe", health = 8}))
     print("init complete")
 end
 
-function handle_damage(sprite)
-    if Sprite.get_health(sprite) == 0 then
-        print(Sprite.get_name(sprite).." has died!")
-    elseif Sprite.get_health(sprite) == 1 then
-        print(Sprite.get_name(sprite).." is not looking so good..")
+function handle_damage(index)
+    if Sprite.get_health(sprites[index]) == 0 then
+        print(Sprite.get_name(sprites[index]).." has died!")
+        table.remove(sprites, index)
+    elseif Sprite.get_health(sprites[index]) == 1 then
+        print(Sprite.get_name(sprites[index]).." is not looking so good..")
     else
-        print(Sprite.get_name(sprite).." was damaged!")
+        print(Sprite.get_name(sprites[index]).." was damaged!")
     end
 end
